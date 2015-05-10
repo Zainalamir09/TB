cf082ce714effa3056f1748ba99903aa32f3f7c0
Zainalamir09 <zainal0909@gmail.com>
2015-05-10 13:39:36 +0700

    Create output.java

---
+++ src/tb/output.java
@@ -0,0 +1,11 @@
+
+package tb;
+
+public class output {
+
+    Analisis r= new Analisis();
+    public void tampil (){
+            System.out.println("hasil :"+r.hitung());
+            System.out.println("Hasil : "+r.hitung_rasio());
+}
+}
