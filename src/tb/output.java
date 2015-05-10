1704a93331133ebf9571b530e9b472f51d0db7ad
adityanugroho87 <adityanugroho51@yahoo.com>
2015-05-10 21:53:38 +0700

    Update output.java

--- src/tb/output.java
+++ src/tb/output.java
@@ -4,7 +4,7 @@ package tb;
 public class output {
 
     Analisis r= new Analisis();
-    public void tampil (){
+    public void tampil (){ 
             System.out.println("hasil :"+r.hitung());
             System.out.println("Hasil : "+r.hitung_rasio());
             if(r.a==true){
