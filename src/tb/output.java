bc07b811461474096f8865f7033724bd62e7f323
Zainalamir09 <zainal0909@gmail.com>
2015-05-10 18:46:42 +0700

    Update output.java

--- src/tb/output.java
+++ src/tb/output.java
@@ -7,5 +7,79 @@ public class output {
     public void tampil (){
             System.out.println("hasil :"+r.hitung());
             System.out.println("Hasil : "+r.hitung_rasio());
-}
+            if(r.a==true){
+                System.out.println("bentuk : persegi panjang");
+                System.out.println("sesuai");
+            }else{
+                System.out.println("tidak sesuai");
+            }
+            if(r.b==true){
+                System.out.println("sesuai");
+            }else{
+                System.out.println("tidak sesuai");
+            }
+            if(r.c==true){
+                 System.out.println("sesuai");
+            }else{
+             System.out.println("tidak sesuai");
+            }
+            if(r.d==true){
+                 System.out.println("sesuai");
+            }else{
+                  System.out.println("tidak sesuai");
+            }
+            if(r.e==true){
+                System.out.println("sesuai");
+            }else{
+                System.out.println("tidak sesuai");
+            }
+            if(r.f==true){
+                 System.out.println("sesuai");
+            }else{
+             System.out.println("tidak sesuai");
+            }
+            if(r.g==true){
+                System.out.println("sesuai");
+            }else{
+                System.out.println("tidak sesuai");
+             }
+            if(r.h==true){
+                   System.out.println("sesuai");
+                   
+            }else{
+             System.out.println("tidak sesuai");
+            }
+            if(r.i==true){
+                System.out.println("sesuai");
+            }else{
+                 System.out.println("tidak sesuai");
+            }
+            if(r.j==true){
+                     System.out.println("sesuai");
+            }else{
+                 System.out.println("tidak sesuai");
+            }
+            if(r.k==true){
+                 System.out.println("sesuai");
+            }else{
+                  System.out.println("tidak sesuai");
+            }
+            if(r.l==true){
+                  System.out.println("sesuai");
+            }else{
+                System.out.println("tidak sesuai");
+            }
+            if(r.m==true){
+               System.out.println("sesuai");
+
+            }else{
+               System.out.println("tidak sesuai");  
+            }
+            if(r.n==true)
+            {
+                System.out.println("sesuai");
+            }else{
+                System.out.println("tidak sesuai");
+            }
+    }
 }
